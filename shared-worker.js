871d1262f3ffb1de25a52ff2671fe781cc25448b
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2021-10-11 01:24:30 +0300

    starting a port

--- shared-worker.js
+++ shared-worker.js
@@ -1,3 +1,14 @@
+let ports = [], i = 0;
+
 this.addEventListener('connect', function (e) {
-    console.log(e);
+    let port = e.ports[0];
+    ports.push(port);
+    port.start();
+
+    setInterval(() => {
+        i++;
+        ports.forEach(port => {
+            port.postMessage('Sending Message ' + i);
+        });
+    }, 500);
 });
