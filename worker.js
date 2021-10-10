54fcb1f262419e9c0ac63169096bc63325fa069e
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2021-10-11 01:06:59 +0300

    importScripts method

--- worker.js
+++ worker.js
@@ -1,23 +1,2 @@
-let message = null;
-
-this.addEventListener('message', function (event) {
-    console.log('event.data :>> ', event.data);
-
-    // reference sharing:
-    message = event.data
-});
-
-setTimeout(() => {
-    console.log('Message now: ', message);
-}, 1000);
-
-/*
-Workers APIs
-    Standard (JSON, setTimeout...)
-    WebSockets
-    XHR and Fetch
-    Promise
-    IndexDB
-    Location (partial)
-    Navigator (partial)
-*/
+importScripts('helper.js');
+console.log('this will print after helper loads.');
