f6f9e153e3f27e3e5333ebb33579ee61e4fa8a88
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2021-10-11 01:04:38 +0300

    Workers APIs

--- worker.js
+++ worker.js
@@ -1,3 +1,23 @@
+let message = null;
+
 this.addEventListener('message', function (event) {
     console.log('event.data :>> ', event.data);
+
+    // reference sharing:
+    message = event.data
 });
+
+setTimeout(() => {
+    console.log('Message now: ', message);
+}, 1000);
+
+/*
+Workers APIs
+    Standard (JSON, setTimeout...)
+    WebSockets
+    XHR and Fetch
+    Promise
+    IndexDB
+    Location (partial)
+    Navigator (partial)
+*/
