551073464b48a9f4260aea1b2e9b2a14a909e72c
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2021-10-11 00:59:29 +0300

    functions cannot be passed to workers

--- worker.js
+++ worker.js
@@ -1,9 +1,3 @@
 this.addEventListener('message', function (event) {
     console.log('event.data :>> ', event.data);
 });
-
-let i = 0;
-
-setInterval(function () {
-    this.postMessage('counter value: ' + i++);
-}.bind(this), 500); // bind(this): finds the execution context of the function.
